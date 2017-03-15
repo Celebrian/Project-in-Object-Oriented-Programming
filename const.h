59924ae231088848733c38486299cdfa83d61434
Per-Kristian <pkbuer@stud.ntnu.no>
2017-03-15 13:57:43 +0100

    Added some consts

--- const.h
+++ const.h
@@ -2,6 +2,11 @@
 #define  __CONST_H
 
 //CONST'er:
+const int MAXTXT = 80;
+const int NASJONLEN = 20;
+const int MAXNASJONER = 200;
+const int MAXOVELSER = 20;
+//const int MAXDELTAGERE = ??
 
 #endif
 
